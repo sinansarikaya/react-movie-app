2c48fc43c89b8a7822357ca49b56c4a54724e575
bzceval <busraaceval@gmail.com>
2022-11-12 23:19:38 +0300

    add detail card design

--- src/pages/movieDetail/MovieDetail.styled.jsx
+++ src/pages/movieDetail/MovieDetail.styled.jsx
@@ -25,21 +25,48 @@ export const Aside = styled.div`
   gap: 1rem;
   border-radius: 1rem;
   box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  
   .title {
     background-color: #435058;
     color: #c7cdd3;
     padding: 1rem;
     text-align: center;
-    font-weight: bold;
-    font-size: 1.2rem;
+    font-size: 1.4rem;
+    font-weight: 500;
+    font-family: 'Oswald', sans-serif;
+    
   }
+  .date {
+    color: hsl(0, 45%, 50%);
+    font-family: 'Oswald', sans-serif;
+    text-align: end;
+    padding: 0rem 2.5rem;
+    font-weight: bold;
+    font-size: 13px;
+    padding: 1.5rem 0rem;
+  }
+
   .detail {
     height: 100%;
-    padding: 1rem;
+    padding: 0rem 2.5rem;
     display: flex;
     flex-direction: column;
+    line-height: 2rem; 
+    color: #343a40;
     .overview {
       margin-bottom: auto;
     }
   }
+
+  .subtitle { 
+      width: 100%;
+      font-family: 'Oswald', sans-serif;
+      padding: 1rem 2rem;
+      color: #343a40;
+      gap: 1rem;
+
+      p {
+        margin-bottom: 8px;
+      }
+    }
 `;
